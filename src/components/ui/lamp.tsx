ec24da40c8cdcdb53de1211f2f8ceecb7fedc617
ItsNaunas <naunas@example.com>
2025-10-12 01:50:37 +0100

    fix: move hero content much further down to prevent cutoff

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -13,7 +13,7 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        "relative flex min-h-[80vh] flex-col items-center justify-center overflow-visible bg-black w-full rounded-md z-0 pt-20",
+        "relative flex min-h-[100vh] flex-col items-center justify-center overflow-visible bg-black w-full rounded-md z-0 pb-32",
         className
       )}
     >
@@ -77,7 +77,7 @@ export const LampContainer = ({
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-black "></div>
       </div>
 
-      <div className="relative z-50 flex -translate-y-20 flex-col items-center px-5">
+      <div className="relative z-50 flex translate-y-20 flex-col items-center px-5">
         {children}
       </div>
     </div>
